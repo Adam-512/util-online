ad857c0ee3a191c228b88f2fe65567748b4b33ac
Adam-512 <90313603+Adam-512@users.noreply.github.com>
2022-07-28 14:13:48 +0800

    feat: 倒计时切换tab问题；添加样式

--- util/countdown.js
+++ util/countdown.js
@@ -1,18 +1,31 @@
 import { requestInterval } from "./exctInterval.js";
 
+//main
+let $el = document.getElementById("countdownWrapper");
+let deadline = $el && $el.dataset.deadline;
+let timer = null;
+onVisible(function () {
+  cancelAnimationFrame(timer && timer.value);
+  countdown(deadline);
+});
+countdown(deadline);
+
 function countdown(deadline) {
   let time = (new Date(deadline).getTime() - Date.now()) / 1000;
   let days, hours, minutes, seconds;
-  let $div = document.createElement("DIV");
-  $div.id = "countdownEl";
-  document.documentElement.appendChild($div);
+
   const updateTime = () => {
-    const el = document.getElementById("countdownEl") || $div;
-    let text = `${days} : ${hours} : ${minutes} : ${seconds}`;
-    console.log(text);
-    el.innerText = text;
+    let text = `${addHtml(hours)}:${addHtml(minutes)}:${addHtml(seconds)}`;
+    $el.innerHTML = text;
   };
-  requestInterval(function () {
+  const addHtml = (num) => {
+    let _num = (num + "").split("");
+    if (_num.length === 1) {
+      return `<span>0</span><span>${_num[0]}</span>`;
+    }
+    return `<span>${_num[0]}</span><span>${_num[1]}</span>`;
+  };
+  timer = requestInterval(function () {
     time--;
     if (time > 0) {
       days = Math.floor(time / 60 / 60 / 24);
@@ -24,6 +37,32 @@ function countdown(deadline) {
   }, 1000);
 }
 
-console.log(12);
+function onVisible(cb) {
+  const callback = (event) => {
+    if (document.visibilityState === "visible") {
+      cb(event);
+    }
+  };
+  document.addEventListener("visibilitychange", callback, true);
+}
 
-countdown("2023-7-28 11:00");
+const styles = `
+  #countdownWrapper span{
+    display:inline-block;
+    padding:5px 0;
+    font-size:21px;
+    color:white;
+    background:#000;
+    width:28px;
+    text-align:center;
+  }
+  #countdownWrapper span:nth-child(odd){
+    margin:0 3px;
+  }
+  #countdownWrapper span:nth-child(even):not(:last-child){
+    margin-right:3px;
+  }
+`;
+const styleEl = document.createElement("style");
+styleEl.innerText = styles;
+document.head.appendChild(styleEl);
